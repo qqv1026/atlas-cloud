390f7c5e749df38ee3c759fc6612cc7d52bb6c4b
qqv1026 <qqv1026@gmail.com>
2025-12-12 23:56:18 +0800

    Delete world_causal.py

--- world_causal.py
+++
@@ -1,8 +0,0 @@
-from modules.world_model.causal_model import CausalModel
-
-class WorldCausal:
-    def __init__(self):
-        self.causal = CausalModel()
-
-    def run(self, query: str):
-        return [self.causal.analyze(query)]
