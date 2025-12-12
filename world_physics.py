d1340a375c6faa094d091bf11f11559d92a24983
qqv1026 <qqv1026@gmail.com>
2025-12-12 23:57:05 +0800

    Delete world_physics.py

--- world_physics.py
+++
@@ -1,9 +0,0 @@
-from modules.world_model.physics_model import PhysicsModel
-
-class WorldPhysics:
-    def __init__(self):
-        self.physics = PhysicsModel()
-
-    def run(self, query: str):
-        g = self.physics.gravity()
-        return [f"重力加速度約 {g} m/s^2，與『{query}』相關的物理直觀已載入。"]
