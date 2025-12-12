c2d07b846d688030d2c4258517188bb379463c1c
qqv1026 <qqv1026@gmail.com>
2025-12-12 23:56:41 +0800

    Delete world_commonsense.py

--- world_commonsense.py
+++
@@ -1,8 +0,0 @@
-from modules.world_model.commonsense import CommonSenseModel
-
-class WorldCommonsense:
-    def __init__(self):
-        self.cs = CommonSenseModel()
-
-    def run(self, query: str):
-        return [self.cs.check(query)]
