7f83d1eba8a139353ba5fdac334e4f380c1f76d1
qqv1026 <qqv1026@gmail.com>
2025-12-12 23:51:41 +0800

    Delete memory.py

--- memory.py
+++
@@ -1,8 +0,0 @@
-from modules.memory_core import MemoryCore
-
-class Memory:
-    def __init__(self):
-        self.core = MemoryCore()
-
-    def run(self, query: str):
-        return self.core.get_context()
