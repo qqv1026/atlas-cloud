212517546a0ffc831f996039527ffd3753923aaa
qqv1026 <qqv1026@gmail.com>
2025-12-12 23:53:21 +0800

    Delete reasoning.py

--- reasoning.py
+++
@@ -1,8 +0,0 @@
-from modules.reasoning_core.reasoning_engine import ReasoningEngine
-
-class Reasoning:
-    def __init__(self):
-        self.engine = ReasoningEngine()
-
-    def run(self, query: str):
-        return self.engine.run(query)
